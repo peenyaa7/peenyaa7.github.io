5f1628f991aed94612513cb73a2be606ad7a82dc
Francisco Javier Peña Vela <peenyaa7@gmail.com>
2024-03-04 23:37:40 +0100

    feat: :sparkles: implementa Open Graph y mejoras de visualización en iconos

--- src/sections/TechnologiesSection/TechnologyView.tsx
+++ src/sections/TechnologiesSection/TechnologyView.tsx
@@ -16,16 +16,11 @@ export const TechnologyView = ({ technology }: Props) => {
             onMouseEnter={() => setFocus(true)}
             onMouseLeave={() => setFocus(false)}
         >
-            <img src={technology.icon} alt={technology.name} loading='lazy' id='icon' className={`${technology.name === 'GitHub' && 'github'}`}
+            <img src={technology.icon} alt={technology.name} loading='lazy' id='icon' className={`${(technology.name === 'GitHub' || technology.name === 'Flask') && 'github'}`}
                 style={{
                 height: '80px',
                 padding: '10px',
             }} />
-            {/* <svg
-                fill='white'
-            >
-                <use href={technology.icon} />
-            </svg> */}
             <span style={{
                 // animation: 'fadeIn 1s',
                 opacity: focus ? 1 : 0,
